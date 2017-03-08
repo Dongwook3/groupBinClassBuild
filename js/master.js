8bb27fc4f9c48a8597ac2135db1546a0f5276a72
DONGWOOK SHIN <Derek@dongwookui-air.fc.ca>
2017-03-08 12:29:33 -0500

    modified master.js

---
+++ js/master.js
@@ -0,0 +1,40 @@
+(function() {
+	// expanded AJAX example
+	$('.thumbInfo img').on('click', function() {
+
+		// do an AJAX call
+		$.ajax({
+			url: "includes/ajaxQuery.php", 
+			data: { model: this.id }, //idx
+			type: "GET"
+		}) // don't put a semicolon here so we can chain methods together
+
+		.done(function(data) {
+			console.log(data);
+
+			/*if (data) {
+				data = JSON.parse(data);
+				renderCarInfor(data);
+			} else {
+				alert('your ajax call didn\'t work');
+			}*/
+			data = JSON.parse(data);
+			renderCarInfo(data);
+		}) // don't put a semicolon here either!
+
+		.fail(function(ajaxCall, status, error) {
+			console.log(status, ", ", error);
+			console.dir(ajaxCall); // outputs the ajax call as an object
+		}); // terminate the ajax function
+
+		function renderCarInfo(car) {
+			$('.thumbInfo img').addClass('nonActive'); // for collections, use jQuery (more than oneelement)
+			$('#' + car.model).removeClass('nonActive');
+
+			$('.subhead span').text(" mini Cooper " + car.model);
+			$('.modelName').text(car.modelName);
+			$('.priceInfo').text(car.pricing);
+			$('.modelDetails').text(car.modelDetails);
+		}
+	});
+})();
